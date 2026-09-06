c6e8d88148b89a35e03eceaff67fa963b6be9524
naga sirisha <324103282002.sirisha@gvpcew.ac.in>
2026-09-06 11:56:44 +0530

    initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>AI Cognitive Memory Platform</h1>
+
+      <p>
+        Cognitive gaming and memory assistance for elderly users.
+      </p>
+    </div>
+  );
+}
+
+export default App;
